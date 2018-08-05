2bbed5641ba38597c364c883592af9274d1885bd
lion137 <lion137@gmail.com>
2018-08-05 18:28:30 +0100

    new function to mllib added

--- mllib.py
+++ mllib.py
@@ -4,9 +4,20 @@ from imports import *
 
 
 def display_all(df):
-    """displays wider view of a dataframe df"""
+    """displays wider view of a pandas data frame df"""
     with pd.option_context("display.max_rows", 1000, "display.max_columns", 1000):
         display(df)
+
+
+def make_submission(df_columns, labels):
+    """Takes two the same length lists of columns and labels.
+        Creates a data frame with colums: df_columns
+        and labels: labels, in order of labels list,
+        return the pd.DataFrame created"""
+    data = {key:val for (key, val) in zip(labels, df_columns)}
+    return pd.DataFrame(data, columns=labels)
+
+
 
 
 
