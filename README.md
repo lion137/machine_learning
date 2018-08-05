4f879c1f9415eba7d4cec42c5eb8d096eca85bd2
lion137 <lion137@gmail.com>
2018-08-05 18:30:39 +0100

    new function to mllib added

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ### machine_learning    
 #### Functions:   
-diplay_all - better displays a pandas dataframe   
+diplay_all - better displays a pandas dataframe
+
+def make_submission(df_columns, labels) - takes two the same length lists of columns and labels.
+        Creates a data frame with colums: df_columns
+        and labels: labels, in order of labels list,
+        return the pd.DataFrame created. Used to create a
+        submission frame
 
--- mllib.py
+++ mllib.py
@@ -13,7 +13,8 @@ def make_submission(df_columns, labels):
     """Takes two the same length lists of columns and labels.
         Creates a data frame with colums: df_columns
         and labels: labels, in order of labels list,
-        return the pd.DataFrame created"""
+        return the pd.DataFrame created. Used to create a
+        submission frame"""
     data = {key:val for (key, val) in zip(labels, df_columns)}
     return pd.DataFrame(data, columns=labels)
 
